27233dbe6d208f65c68c83c3f01a5530050dbd1f
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2022-10-31 20:17:55 -0400

    adds nav bar icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { IconComponent } from './icon/icon.component';
-
 const routes: Routes = []
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    IconComponent
   ],
   imports: [
     BrowserModule,
