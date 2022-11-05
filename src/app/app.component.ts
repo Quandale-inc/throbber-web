910da774271e4a25fb841fc0b4926893e9584905
Amir Afshari <a_amirhossein@icloud.com>
2022-11-05 00:48:23 -0400

    adds aside menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'throbber-web';
+  title = 'throbber';
+  isMenuOpen = false;
+  openSideMenu() {
+    this.isMenuOpen = true;
+  }
+  closeSideMenu() {
+    this.isMenuOpen = false;
+  }
 }
